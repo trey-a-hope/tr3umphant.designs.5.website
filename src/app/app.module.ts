ee3ff66cb9a8979ef38e7e83dd791a9c292f1f63
Trey Hope <treyhope@Treys-MacBook-Air.local>
2017-12-22 21:23:08 -0500

    Finally, after fucking around with this shit trying to get a git repo initialized, smh.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MDBBootstrapModule.forRoot()
   ],
+  schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
