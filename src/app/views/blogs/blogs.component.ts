fb802fa9d255e19f6bc96428b8c24846a61b00ff
Trey Hope <treyhope@Treys-MacBook-Air.local>
2018-01-04 18:21:12 -0500

    Share feature working for blogs.

--- src/app/views/blogs/blogs.component.ts
+++ src/app/views/blogs/blogs.component.ts
@@ -21,8 +21,25 @@ export class BlogsComponent implements OnInit {
     });
   }
 
-  public share(): void {
-    alert('Share Blog');
+  public share(provider: string, blogUrl: string): void {
+    var text: string = 'Check out this blog I found on td5.tr3umphant-designs.com!';
+
+    switch (provider) {
+        case 'TWITTER':
+            window.open('http://twitter.com/share?url='+encodeURIComponent(blogUrl)+'&text='+encodeURIComponent(text), '', 'left=0,top=0,width=550,height=450,personalbar=0,toolbar=0,scrollbars=0,resizable=0');
+            break;
+        case 'FACEBOOK':
+            window.open('http://facebook.com/sharer/sharer.php?u='+encodeURIComponent(blogUrl)+'&title='+encodeURIComponent(text), '', 'left=0,top=0,width=650,height=420,personalbar=0,toolbar=0,scrollbars=0,resizable=0');
+            break;
+        case 'LINKEDIN':
+            window.open('http://www.linkedin.com/shareArticle?mini=true&url='+encodeURIComponent(blogUrl)+'&text='+encodeURIComponent(text), '', 'left=0,top=0,width=650,height=420,personalbar=0,toolbar=0,scrollbars=0,resizable=0');
+            break;
+        case 'GOOGLE+':
+            window.open('https://plus.google.com/share?url='+encodeURIComponent(blogUrl)+'&text='+encodeURIComponent(text), '', 'left=0,top=0,width=650,height=420,personalbar=0,toolbar=0,scrollbars=0,resizable=0');
+            break;
+        default:
+            break;
+    }
   }
 
 }
